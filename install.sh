4d81afb4c4f977d1657a32d9f686f1084df49b4c
Leonardo Padilha Guarezi <leonardo_guarezi@hotmail.com>
2022-03-31 15:04:04 -0300

    ADD files from old project

---
+++ install.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+DWM="dwm-6.2"
+DMENU="dmenu-5.0"
+ST="st-0.8.4"
+PROJECT="lpgMint"
+
+INIT_PACK=("git" "curl" "wget" "sudo")
+DEPS_PACK=("vim" "make" "build-essential" "libx11-dev" "libxft-dev" "libxinerama-dev" "fonts-font-awesome" "feh")
+POST_PACK=("tmux" "net-tools" "python3" "htop" "jq" "cmake" "tcpdump")
+
+function printc(){
+	CLEAR_COLOR="\033[0m"
+	l=$2
+	msg=$1
+	if [ "$l" = "s" ];then # success
+		PRIMARY_COLOR="\033[36;01m"
+	fi
+	if [ "$l" = "w" ];then # warning
+		PRIMARY_COLOR="\033[33;01m"
+	fi
+	if [ "$l" = "e" ];then # error
+		PRIMARY_COLOR="\033[31;01m"
+	fi
+	if [ "$l" = "i" ];then # info
+		PRIMARY_COLOR="\033[34;01m"
+	fi
+	if [ "$l" = "l" ];then # log
+		PRIMARY_COLOR="\033[0;01m"
+	fi
+	if [ "$l" = "d" ];then # default log
+		PRIMARY_COLOR="\033[0m"
+	fi
+
+	printf "$PRIMARY_COLOR$msg$CLEAR_COLOR"
+}
+
+function check_root(){
+	# Check if is root user
+	if [[ ! "$(id -u)" -eq "0" ]];then
+		printc "\nError: Root user is required!\n\n" "e"
+		exit 1
+	fi
+}
+
+check_root
+
+printc "\nStarting installation...\n" "i"
+printc "  Inform the current user: " "l"
+read USERNAME
+
+if [ -z "$USERNAME" ]; then
+	printc "User name can't be empity!\n" "e"
+	exit 1
+fi
+
+id "$USERNAME" &>/dev/null
+if [ ! "$?" -eq "0" ]; then
+	printc "User not found!\n" "e"
+	exit 1
+fi
+
+U_FOLDER="/home/$USERNAME"
+
+printc "  Installing base packages\n" "i"
+apt install ${INIT_PACK[@]} -y
+
+printc "  Installing dependencis\n" "i"
+sudo apt install ${DEPS_PACK[@]} -y
+
+U_FOLDER="/home/$USERNAME"
+S_FOLDER="$U_FOLDER/$PROJECT/suckless"
+CF_FOLDER="$U_FOLDER/$PROJECT/config-files"
+SC_FOLDER="$U_FOLDER/$PROJECT/scripts"
+
+printc "  Compile and install [dwm-6.2]\n" "i"
+cd "$S_FOLDER/$DWM" && sudo make clean install
+printc "  Compile and install [st-0.8.4]\n" "i"
+cd "$S_FOLDER/$ST" && sudo make clean install
+printc "  Compile and install [dmenu-5.0]\n" "i"
+cd "$S_FOLDER/$DMENU" && sudo make clean install
+
+printc "  Compile and install [pfetch]\n" "i"
+cd "$SC_FOLDER/pfetch" && sudo make install
+
+printc "  Creating configurations files\n" "i"
+# .dwm autostart
+cp -r "$SC_FOLDER/.dwm" "$U_FOLDER/"
+sudo chown  "$USERNAME:$USERNAME" "$U_FOLDER/.dwm/autostart.sh"
+# .tmux
+bash -c  "$(wget -qO- https://git.io/JCbIh)"
+sudo chown  "$USERNAME:$USERNAME" "$U_FOLDER/.tmux.conf"
+# .vimrc
+bash -c  "$(wget -qO- https://git.io/JCbTi)"
+sudo chown  "$USERNAME:$USERNAME" "$U_FOLDER/.vimrc"
+# dwm entry
+sudo cp "$CF_FOLDER/dwm.desktop" "/usr/share/xsessions/"
+
+printc "  Creating some scripts\n" "i"
+cp "$SC_FOLDER/fehbg" "/bin/fehbg"
+cp "$SC_FOLDER/pmenu" "/bin/pmenu"
+cp "$SC_FOLDER/helpdwm" "/bin/helpdwm"
+cp -r "$SC_FOLDER/dwm-help" "/bin/dwm-help"
+
+printc "  Copy wallpapers\n" "i"
+sudo mkdir "/usr/share/wallpapers/lpgMint"
+sudo cp -a "$U_FOLDER/$PROJECT/img/wallpapers/." "/usr/share/wallpapers/lpgMint"
+
+printc "  Copy Gruvbox theme\n" "i"
+sudo cp -rf "$U_FOLDER/$PROJECT/theme/gruvbox-material-gtk/themes/." "/usr/share/themes"
+sudo cp -rf "$U_FOLDER/$PROJECT/theme/gruvbox-material-gtk/icons/." "/usr/share/icons"
+sudo gtk-update-icon-cache "/usr/share/icons/Gruvbox-Material-Dark"
+
+printc "  Setup Gruvbox theme\n" "i"
+cp "$CF_FOLDER/gtk-3.0/settings.ini" "$U_FOLDER/.config/gtk-3.0/"
+sudo chown  "$USERNAME:$USERNAME" "$U_FOLDER/.config/gtk-3.0/settings.ini"
+
+printc "  Installing programs\n" "i"
+sudo apt install ${POST_PACK[@]} -y
+
+printc "Cleaning files and prepering for reboot\n" "s"
+
+sudo apt autoremove
+
+printc "\nInstalation finished \\o/\n" "s"
+
