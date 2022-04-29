e1aa4c468ee33f5b8c6b239b4fbd5ba6d5ec91f5
Leonardo Padilha Guarezi <lguarezi@positivo.com.br>
2022-04-29 10:01:05 -0300

    Atualiznafo autostart do dwm

--- config-files/.dwm/autostart.sh
+++ config-files/.dwm/autostart.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-dwm_date(){
+dwm_date() {
 	DATA=$(date +"%F %R")
 	echo "[  ${DATA} ]"
 }
 
 dwm_weather() {
-    LOCATION=Curitiba
-
-	DATA=$(curl -s wttr.in/$LOCATION?format=1 | grep -o ".[0-9].*")
-	echo "[ ${DATA} ]"
+	DATA=$(curl -s wttr.in?format=1 | grep -o "[+\-][0-9].*")
+	if [ "$DATA" == "" ]; then
+		echo "[ null ]"
+	else
+		echo "[ ${DATA} ]"
+	fi
 }
-
 dwm_hdd(){
 	free="$(df -h /home | grep /dev | awk '{print $3}')"
 	total="$(df -h /home | grep /dev | awk '{print $2}')"
@@ -25,6 +26,26 @@ dwm_pkgupdates() {
 	total="$(dpkg -l | grep -c ^i)"
 
 	echo "[  $upgrds/$total ]"
+}
+
+dwm_getBatteryPerc() {
+	perc=$(cat /sys/class/power_supply/BAT0/capacity)
+	isCharging=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "state:               discharging" | wc -l)
+
+	if [ ! $isCharging -eq 1 ]; then
+		icon=""
+	elif [ $perc -gt 75 ]; then
+		icon=""
+	elif [ $perc -gt 50 ]; then
+		icon=""
+	elif [ $perc -gt 25 ]; then
+		icon=""
+	elif [ $perc -gt 15 ]; then
+		icon=""
+	elif [ $perc -gt 5 ]; then
+		icon=""
+	fi
+	echo "[ $icon $perc% ]"
 }
 
 dwm_resources () {
@@ -66,15 +87,19 @@ while true; do
 
 	count_weather=$((count_weather + 1))
 	count_hdd=$((count_hdd + 1))
-	xsetroot -name "$(dwm_resources) $hdd $pkgupdate $weather $(dwm_date)"
+	xsetroot -name "$(dwm_resources) $hdd $pkgupdate $weather $(dwm_getBatteryPerc) $(dwm_date)"
 	sleep 30s
 done &
 
-if [ $(xrandr | grep " connected " | awk '{ print$1  }' | wc -l) -eq 2 ]; then
+if [ $(xrandr | grep " connected " | awk '{ print$1  }' | grep "HDMI-" | wc -l) -eq 1 ]; then
 	xrandr --output HDMI-2 --mode 1920x1080 --output eDP-1 --mode 1920x1080 --right-of HDMI-2
+elif [ $(xrandr | grep " connected " | awk '{ print$1  }' | grep "DP-" | wc -l) -gt 1 ]; then
+	xrandr --output DP-1 --mode 1366x768 --output eDP-1 --mode 1920x1080 --right-of DP-1
 else
 	xrandr --output eDP-1 --mode 1920x1080
 fi
+
 setxkbmap -model abnt2 -layout br -variant abnt2
 fehbg
+teams
 exec dwm
