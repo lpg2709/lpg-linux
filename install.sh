7ea779baea0d22571f197bd5134c8d8d4cce4f28
Leonardo Padilha Guarezi <leonardo_guarezi@hotmail.com>
2023-01-24 17:23:03 -0300

    ADD nvim compilation

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ USER_NAME="${SUDO_USER:-$USER}"
 
 DEPS_PACK=("vim" "make" "build-essential" "libx11-dev" "libxft-dev")
 POST_PACK=("tmux" "net-tools" "python3" "htop" "jq" "firefox-esr" "cmake" "tcpdump")
+NVIM_COMPILE_DEPS=("ninja-build" "gettext" "libtool" "libtool-bin" "autoconf" "automake" "g++" "pkg-config" "unzip" "doxygen" "ripgrep")
 
 PROGRAMS_FOLDER="$USER_HOME/$PROJECT/programs"
 SUCKLESS_FOLDER="$USER_HOME/$PROJECT/suckless"
@@ -39,6 +40,19 @@ cd "$SUCKLESS_FOLDER/st" && sudo make clean install
 
 printc "  Compile and install [pfetch]\n" "i"
 cd "$PROGRAMS_FOLDER/pfetch" && sudo make install
+
+printc "  Check if neovim is installed ...\n" "i"
+nvim --version > /dev/null 2>&1
+if [ ! $(echo $?) -eq 0 ]; then
+	printc "  Installing nvim dependencis\n" "i"
+	sudo apt install ${NVIM_COMPILE_DEPS[@]} -y
+
+	printc "  Install nvim\n" "i"
+	git clone https://github.com/neovim/neovim "/tmp/neovim"
+	cd "/tmp/neovim" && git checkout stable && make CMAKE_BUILD_TYPE=Release && sudo make install
+else
+	printc "  neovim is installed. \n" "i"
+fi
 
 printc "  Returning to project folder\n" "i"
 cd "$USER_HOME/$PROJECT"
