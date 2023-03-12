b24c1c1ec83238abb84e8f4d02341784db85cede
lpg2709 <leonardo_guarezi@hotmail.com>
2023-03-12 14:30:21 -0300

    Update suckless confgis

--- suckless/dwm-6.2/config.def.h
+++ suckless/dwm-6.2/config.def.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "7", "8", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,7 +29,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Thunar",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Thunar",   NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Steam",    NULL,       NULL,       1 << 5,       1,           -1 },
 };
 
 /* layout(s) */
--- suckless/st-0.8.4/config.def.h
+++ suckless/st-0.8.4/config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "fontawesome:pixelsize=15:antialias=true:autohint=true" };
+static char *font = "TerminessTTF Nerd Font Mono:style=Bold:pixelsize=20:antialias=true:autohint=true";
+static char *font2[] = { "Monospace:pixelsize=15:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
