864f9e678947a737787209b4487ac087bbdd90e0
Leonardo Padilha Guarezi <leonardo_guarezi@hotmail.com>
2024-03-10 14:27:48 -0300

    ADD rules for Steam and Gimp

--- suckless/dwm-6.4/config.def.h
+++ suckless/dwm-6.4/config.def.h
@@ -32,10 +32,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 6,       1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Thunar",   NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "Steam",    NULL,       NULL,       1 << 5,       1,           -1 },
+	{ "Steam",    NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
