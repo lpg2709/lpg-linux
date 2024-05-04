a35b8bfe5242b160f3b77f25936030381ad5fb41
Leonardo  Padilha Guarezi <leonardo_guarezi@hotmail.com>
2024-05-04 17:23:40 +0000

    FIX upgrade count

--- configs/.dwm/autostart.sh
+++ configs/.dwm/autostart.sh
@@ -14,7 +14,7 @@ dwm_hdd(){
 }
 
 dwm_pkgupdates() {
-	upgrds="$(xbps-install -un | cut -d' ' -f2 | grep update | wc -l)"
+	upgrds="$(xbps-install -Sun | cut -d' ' -f2 | grep update | wc -l)"
 	total="$(xbps-query -l | wc -l)"
 
 	echo "[  $upgrds/$total ]"
